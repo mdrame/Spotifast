cd916b2aa9c1812a6772d0ebd2464d873dec78a0
mohammed <mdrame113@gmail.com>
2020-12-01 11:53:28 -0500

    autorization initiated

---
+++ Spotifast/Spotifast/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Spotifast
+//
+//  Created by Mohammed Drame on 12/1/20.
+//
+
+import Foundation
+
+struct  Constants {
+    static let clientID = "4e8b040252c9403ab42423ffc6bb61b1"
+    static let redirectURI = URL(string: "uncover://")!
+    static let client_secret = "e76adfe21cf4ed183cd49c529c6b"
+}
--- Spotifast/Spotifast/ViewController.swift
+++ Spotifast/Spotifast/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ASWebAuthenticationPresentationContextProviding {
+    
+    // Protocol
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return self.view.window ?? ASPresentationAnchor()
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://accounts.spotify.com/authorize?client_id=\(Constants.clientID)&response_typecode&redirect_url=\(Constants.redirectURI)")!
+        let scheme = "auth"
+        let session = ASWebAuthenticationSession(url: url, callbackURLScheme: scheme)
+        {   callbackURL, error in
+            guard error == nil, let callbackURL = callbackURL else { return }
+            let queryItems = URLComponents(string: callbackURL.absoluteString)?.queryItems
+            guard let requestToken = queryItems?.first(where: { $0.name == "code" })?.value else { return }
+            print("Code \(requestToken)")
+            self.startSession(code: requestToken)
+        }
+        session.presentationContextProvider =  self
+        session.start()
     }
     
+    func startSession(code: String) {
+        print("Session started")
+    }
     
 
 
